69c1389296276a216fc987be19ce5e6ea136416b
apoorvnema <apoorvnema38@gmail.com>
2024-04-27 21:25:09 +0530

    Added Realtime update

--- public/js/messages.js
+++ public/js/messages.js
@@ -1,5 +1,36 @@
 const form = document.querySelector("form");
 const token = localStorage.getItem("token");
+let previousMessages = [];
+
+function arraysEqual(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
+}
+
+async function fetchAllMessages() {
+    const pre = document.querySelector("pre");
+    try {
+        const response = await axios.get("http://127.0.0.1:3000/all", { headers: { "Authorization": token } });
+        const newMessages = response.data.message;
+        if (!arraysEqual(previousMessages, newMessages)) {
+            let content = '';
+            const onlineUsersResponse = await axios.get("http://127.0.0.1:3000/online-users", { headers: { "Authorization": token } });
+            const onlineUsers = onlineUsersResponse.data.message.map(user => `${user} joined`).join('\n');
+            content += onlineUsers + '\n';
+            newMessages.forEach((message) => {
+                content += `${message.sender}: ${message.message}\n`;
+            });
+            pre.innerHTML = content;
+            previousMessages = newMessages.slice();
+        }
+    } catch (error) {
+        console.error("Error fetching messages:", error);
+    }
+}
+
 
 form.addEventListener("submit", async (e) => {
     try {
@@ -19,16 +50,8 @@ document.addEventListener("DOMContentLoaded", async () => {
             alert("You are not logged in!");
             document.location.href = "/login";
         };
-        const pre = document.querySelector("pre");
-        pre.innerHTML = "";
-        const onlineUsers = await axios.get("http://127.0.0.1:3000/online-users", { headers: { "Authorization": token } });
-        onlineUsers.data.message.forEach((user) => {
-            pre.innerHTML += `${user} joined\n`;
-        });
-        const messages = await axios.get("http://127.0.0.1:3000/all", { headers: { "Authorization": token } });
-        messages.data.message.forEach((message) => {
-            pre.innerHTML += `${message.sender}: ${message.message}\n`;
-        });
+        fetchAllMessages();
+        setInterval(fetchAllMessages, 1000);
     }
     catch (err) {
         console.error(err);
